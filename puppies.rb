45949bd65f7e3220fda2d0ee39a901756e5bced4
Reaganhatt <reagan.hatt@gmail.com>
2018-06-19 20:29:06 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -35,10 +35,8 @@ less_puppies(9)
 # BONUS
 # 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
 def new_animal(int,int_2)
-  if int > 1 
-    puts "slow down."
-  if int_2 > 1 
-    puts "Nah Rachel."
+  if int > 1 || int_2 >1
+    puts "WHOA. Slow down Rachel."
   else 
     puts "Good job, Rachel."
   end
